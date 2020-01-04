a0ba7ecfd8367b357f675af7a3547cf324402672
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2020-01-04 19:27:25 +0100

    style
    style

---
+++ chinees/includes/inc_head.php
@@ -0,0 +1,13 @@
+<?php
+echo '<title>Chinese Gezondheidsleer</title>
+<META NAME="description" CONTENT="Chinese Gezondheidsleer"/>
+<META NAME="keywords" CONTENT="Chinese Gezondheidsleer">
+<META NAME="copyright" CONTENT="2020, Wiebe de Boer">
+<META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
+<meta http-equive="content-type" content="text/html; charset=ISO-8859-1">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" type="text/css" href="styles/styling.css">
+<meta name=viewport content="width=device-width, initial-scale=1">
+<script type="text/javascript" src="scripts/jquery.js"></script>
+<script type="text/javascript" src="scripts/script.js"></script>';
+?>

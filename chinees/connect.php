5c82929d316d1fbce693e57abeb1a9cd582fe6a6
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2020-01-04 19:27:48 +0100

    db con
    db con

---
+++ chinees/connect.php
@@ -0,0 +1,19 @@
+<?php
+//DATABASE CONNECTION VARIABLES
+$myserver ="localhost";
+$myname = "geennaam";
+$mypassword = "watte2017";
+$mydb = "test_nl";
+
+$conn = new mysqli($myserver, $myname, $mypassword, $mydb);
+if ($conn->connect_error)
+  {
+echo "<P class='error'>Could not select database</P>";
+$connected = 0;
+  }
+else {
+
+$connected = 1;
+
+}
+?>

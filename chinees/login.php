002f97b63b522ade934e552db0c37984100e7ac7
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2020-01-04 14:45:14 +0100

    initial
    initial

---
+++ chinees/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<?php
+include("includes/inc_head.php");
+?>
+</HEAD>
+<BODY>
+<div class="body">
+<div id="main">
+<?php
+include("includes/inc_main.php");
+?>
+</div>
+<div id="footer">
+<?php
+include("includes/inc_bottom.php");
+?>
+</div>
+</div>
+</BODY>
+</HTML>
